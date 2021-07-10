9821b66c4a9d91995b9539f52aa89c5d31941b5c
Eduardo Soares <eduardodeiturama@hotmail.com>
2021-07-10 20:52:57 -0300

    Buildando projeto e batendo no My Json Server quando em produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const baseURL = isDevelopment ? 'http://localhost:3333' : 'https://my-json-server.typicode.com/joseribeiroejrs/movie-list-ignite'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL,
 });
